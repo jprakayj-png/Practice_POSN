061d40cace27ed8e69fe587c1d85f998c56f3a3c
Jarukit Prakayjindawong <jprakayj@gmail.com>
2026-08-26 21:24:33 +0700

    Add files via upload

---
+++ Atcoder/B/B-Count_distinct.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+set<int>s ;
+
+int main() {
+    ios_base::sync_with_stdio(0) ;
+    cin.tie(0) ;
+    int n ;
+    cin >> n ;
+    for (int i = 0 ; i < n ; i++){
+        int x ;
+        cin >> x ;
+        s.insert(x) ;
+    }
+    cout << s.size() << "\n" ;
+    return 0 ;
+}
