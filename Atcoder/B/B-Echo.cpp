152337748d24dc0f3a7646bdb6adae76d95ab0d6
Jarukit Prakayjindawong <jprakayj@gmail.com>
2026-08-29 19:40:00 +0700

    Add files via upload

---
+++ Atcoder/B/B-Echo.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    ios_base::sync_with_stdio(0) ;
+    cin.tie(0) ;
+    int n ;
+    cin >> n ;
+    string s ;
+    cin >> s ;
+    if (n % 2 != 0){
+        cout << "No" ;
+    }
+    else {
+        for (int i = 0 ; i < n / 2 ; i++){
+            if (s[i] != s[i + n / 2]){
+                cout << "No" ;
+                return 0 ;
+            }
+        }
+        cout << "Yes" ;
+        return 0 ;
+    }
+}
