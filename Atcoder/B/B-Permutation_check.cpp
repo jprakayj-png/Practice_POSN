773ae831485fba322d7720a1f041c35e6d15a1f7
Jarukit Prakayjindawong <jprakayj@gmail.com>
2026-08-29 21:06:33 +0700

    Add files via upload

---
+++ Atcoder/B/B-Permutation_check.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+vector<int>v ;
+int main() {
+    ios_base::sync_with_stdio(0) ;
+    cin.tie(0) ;
+    int n ;
+    cin >> n ;
+    v.resize(n) ;
+    for (int i = 0 ; i < n ; i++){
+        cin >> v[i] ;
+    }
+    sort(v.begin(),v.end()) ;
+    for (int i = 1 ; i <= n ; i++){
+        if (i != v[i-1]){
+            cout << "No" ;
+            return 0 ;
+        }
+    }
+    cout << "Yes" ;
+    return 0 ;
+}
